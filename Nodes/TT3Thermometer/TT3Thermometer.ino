a90cdc3cce704decb0a42aa5f205b98d0615ec41
ThoCoder <thomy_h@gmx.de>
2016-11-22 21:59:13 +0100

    added BMP180 support

--- Nodes/TT3Thermometer/TT3Thermometer.ino
+++ Nodes/TT3Thermometer/TT3Thermometer.ino
@@ -2,17 +2,27 @@
 //#define USESERIAL2
 //#define LEDFLASHS
 
+#define DHT22present 1
+#define BMP180present 1
 #define RF69_COMPAT 1
 #include <JeeLib.h>
 
-#define DHT22_NO_FLOAT
-#include <DHT22.h>
-
 #define LED 7
+
+#if defined(DHT22present) && defined(BMP180present)
+#define DHT22_POWER 3
+#define DHT22_PIN 8
+#define BMP180_SCL 9
+#define BMP180_SDA 10
+#elif defined(DHT22present)
 #define DHT22_PIN 9
 #define DHT22_POWER 10
+#elif defined(BMP180present)
+#define BMP180_SCL 9
+#define BMP180_SDA 10
+#endif
 
-#define myNodeID 19
+#define myNodeID 18
 #define network 99
 #define freq RF12_868MHZ
 #define ACK_TIME 50
@@ -22,29 +32,55 @@
 #define WAITINTERVAL 59000
 
 #define CMD_temperature 11
+#define CMD_temperature2 13
+#define CMD_pressure 14
 #define CMD_humidity 16
 #define CMD_PowerSupply 252
 #define CMD_SenderRSSI 196
 
+#if defined(DHT22present)
+#define DHT22_NO_FLOAT
+#include <DHT22.h>
 DHT22 dht22(DHT22_PIN);
+#endif
+
+#if defined(BMP180present)
+#include <PortsBMP085.h>
+PortI2C i2c(1); // BMP085 SDA to D10 and SCL to D9
+BMP085 bmp180(i2c, 3); // ultra high resolution
+#endif
 
 struct DataPacket
 {
-	byte tempCmd;
-	int16_t temp;		// T * 10
-	byte humidityCmd;
-	byte humidity;		// H * 1
 	byte powerCmd;
 	uint16_t power;		// mV * 10
 	byte senderRssiCmd;
 	byte senderRssi;	// dB * -1
+#if defined(DHT22present)
+	byte tempCmd;
+	int16_t temp;		// °C * 10
+	byte humidityCmd;
+	byte humidity;		// % * 1
+#endif
+#if defined(BMP180present)
+	byte temp2Cmd;
+	int16_t temp2;		// °C * 10
+	byte pressureCmd;
+	int16_t pressure;	// hPa * 10
+#endif
 
 	DataPacket()
 	{
-		tempCmd = CMD_temperature;
-		humidityCmd = CMD_humidity;
 		powerCmd = CMD_PowerSupply;
 		senderRssiCmd = CMD_SenderRSSI;
+#if defined(DHT22present)
+		tempCmd = CMD_temperature;
+		humidityCmd = CMD_humidity;
+#endif
+#if defined(BMP180present)
+		temp2Cmd = CMD_temperature2;
+		pressureCmd = CMD_pressure;
+#endif
 	}
 };
 
@@ -198,35 +234,54 @@ void setup()
 	pinMode(LED, OUTPUT);
 	digitalWrite(LED, HIGH);
 
+#if defined(DHT22present)
 	pinMode(DHT22_POWER, OUTPUT);
 	digitalWrite(DHT22_POWER, LOW);
+#endif
 
 #if defined(USESERIAL)
 	Serial.begin(38400);
 	Serial.println("setup");
 #endif
 
+#if defined(BMP180present)
+	Sleepy::loseSomeTime(50);
+	bmp180.getCalibData();
+#endif
+
 	rf12_initialize(myNodeID, freq, network);
 	rf12_sleep(RF12_SLEEP);
 
-	delay(1000);
+	Sleepy::loseSomeTime(1000);
 	digitalWrite(LED, LOW);
 }
 
 void loop() 
 {
+	// MEASURE
+#if defined(DHT22present)
 #if defined(USESERIAL)
-	Serial.println("WAKE UP ...");
+	Serial.println("DHT22");
 #endif
 	digitalWrite(DHT22_POWER, HIGH);
 	Sleepy::loseSomeTime(2000); // DHT22 requires minimum 2s warm-up after power-on.
-
-#if defined(USESERIAL)
-	Serial.println("MEASURE ...");
-#endif
 	DHT22_ERROR_t errorCode = dht22.readData();
 	digitalWrite(DHT22_POWER, LOW);
+#endif
 
+#if defined(BMP180present)
+#if defined(USESERIAL)
+	Serial.println("BMP180");
+#endif
+	Sleepy::loseSomeTime(bmp180.startMeas(BMP085::TEMP));
+	bmp180.getResult(BMP085::TEMP);
+
+	Sleepy::loseSomeTime(bmp180.startMeas(BMP085::PRES));
+	bmp180.getResult(BMP085::PRES);
+#endif
+
+	// CALCULATE
+#if defined(DHT22present)
 	if (errorCode == DHT_ERROR_NONE)
 	{
 		data.temp = dht22.getTemperatureCInt();
@@ -237,14 +292,33 @@ void loop()
 		data.temp = -1000;
 		data.humidity = 100 + errorCode;
 	}
+#endif
 
+#if defined(BMP180present)
+	int16_t temp2;
+	int32_t pressure;
+	bmp180.calculate(temp2, pressure);
+	data.temp2 = temp2;
+	data.pressure = pressure / 10;
+#endif
+
+	// MEASURE VCC
 	data.power = readVcc() * 10;
 
+	// TRACE
 #if defined(USESERIAL)
+#if defined(DHT22present)
 	Serial.print(" T=");
 	Serial.print(data.temp, DEC);
 	Serial.print(" H=");
 	Serial.print(data.humidity, DEC);
+#endif
+#if defined(BMP180present)
+	Serial.print(" T2=");
+	Serial.print(data.temp2, DEC);
+	Serial.print(" P=");
+	Serial.print(data.pressure, DEC);
+#endif
 	Serial.print(" V=");
 	Serial.print(data.power, DEC);
 	Serial.print(" RSSI(-");
@@ -253,8 +327,9 @@ void loop()
 	Serial.println();
 #endif
 
+	// SEND
 #if defined(USESERIAL)
-	Serial.println("SEND ...");
+	Serial.println("SEND");
 #endif
 	word retryDelay = RETRYDELAY;
 	byte retry = 0;
@@ -290,7 +365,7 @@ void loop()
 #endif
 
 #if defined(USESERIAL)
-		Serial.print(" SENT SUCCESSFULLY. R=");
+		Serial.print(" SENT OK. R=");
 		Serial.print(retry, DEC);
 		Serial.println();
 #endif
@@ -302,15 +377,16 @@ void loop()
 #endif
 
 #if defined(USESERIAL)
-		Serial.print(" FAILED TO SEND AFTER ");
+		Serial.print(" SEND ERR AFTER ");
 		Serial.print(RETRIES, DEC);
 		Serial.print(" RETRIES.");
 		Serial.println();
 #endif
 	}
 
+	// SLEEP
 #if defined(USESERIAL)
-	Serial.println("SLEEP ...");
+	Serial.println("SLEEP");
 #endif
 	for (int w = 0; w < WAITLOOPS; w++)
 		Sleepy::loseSomeTime(WAITINTERVAL);
